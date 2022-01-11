d3c2dbb2eaa9c794793bd2ca9c870bcf06751794
ThienPham-Si <82398747+ThienPham-Si@users.noreply.github.com>
2022-01-11 15:08:13 -0600

    Update cartoonize.py

--- cartoonize.py
+++ cartoonize.py
@@ -1,17 +1,14 @@
-import cv2 #for image processing
-import numpy as np #to store image
+import cv2
+import numpy as np 
 import sys
 import os
 from pathlib import Path
 
 def color_quantization(img, k):
-# Transform the image
   data = np.float32(img).reshape((-1, 3))
 
-# Determine criteria
   criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
 
-# Implementing K-Means
   ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
   center = np.uint8(center)
   result = center[label.flatten()]
@@ -40,13 +37,13 @@ def cartoonify(ImagePath, ImageName):
 
     return save(cartoon, ImagePath, ImageName)
 
-def save(ReSized6, ImagePath, ImageName):
+def save(image, ImagePath, ImageName):
     # path1 = os.path.dirname(ImagePath)
     # extension=os.path.splitext(ImagePath)[1]
     BASE_DIR = Path(__file__).resolve().parent.parent
     # path = os.path.join(path1, extension)
     #
-    # cv2.imwrite(path, cv2.cvtColor(ReSized6, cv2.COLOR_RGB2BGR))
+    # cv2.imwrite(path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
     path1 = os.path.dirname(ImagePath)
     # extension=os.path.splitext(ImagePath)[1]
@@ -55,7 +52,7 @@ def save(ReSized6, ImagePath, ImageName):
     newName="result"
     STATIC_DIR = os.path.join(BASE_DIR, 'cartoon\\static', newName+extension)
     print(STATIC_DIR)
-    cv2.imwrite(STATIC_DIR, cv2.cvtColor(ReSized6, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(STATIC_DIR, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
 output = cartoonify(sys.argv[1], sys.argv[2])
 # cv2.imshow('img', org)
